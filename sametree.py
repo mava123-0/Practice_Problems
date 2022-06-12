b0805623ec52cd3cdf46616f2ada0a4f2e4d1c46
Jayanth Menon <83857943+mava123-0@users.noreply.github.com>
2022-06-12 11:27:13 +0530

    Add files via upload

---
+++ sametree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        q1=self.inorderF1(p)
+        q2=self.inorderF2(q)
+        print(q1)
+        print(q2)
+        if(q1==q2):
+            return True
+        return False
+        
+    def inorderF1(self,p):
+        que=[]
+        if(p!=None):
+            que=self.inorderF1(p.left)
+            que.append(p.val)
+            que=que+self.inorderF1(p.right)
+        que.append('null')
+        return que
+            
+    def inorderF2(self,q):
+        que=[]
+        if(q!=None):
+            que=self.inorderF1(q.left)
+            que.append(q.val)
+            que=que+self.inorderF1(q.right)
+        que.append('null')
+        return que
+        
+    
