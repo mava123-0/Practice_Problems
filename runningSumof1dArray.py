c27fd9dc2536ccedd0791934902bcf2391035f36
Jayanth Menon <83857943+mava123-0@users.noreply.github.com>
2022-09-22 23:39:45 +0530

    Arrays

---
+++ runningSumof1dArray.py
@@ -0,0 +1,7 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sum = 0
+        for i in range (len(nums)):
+            sum += nums[i]
+            nums[i] = sum
+        return nums
