d21c8c84bb7b1ac1fdbdbe247acc76823ad94360
Jayanth Menon S <83857943+mava123-0@users.noreply.github.com>
2022-10-07 21:18:20 +0530

    Reverse Linked List

---
+++ LinkedLists/GFGreverseLinkedLisy.py
@@ -0,0 +1,71 @@
+#function Template for python3
+
+"""
+# Node Class
+
+class node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+"""
+
+class Solution:
+    #Function to reverse a linked list.
+    def reverseList(self, head):
+        # Code here
+        temp = head
+        valarr = []
+        while(temp!=None):
+            valarr.append(temp.data)
+            temp = temp.next
+        temp2 = head
+        valarr.reverse()
+        for i in range(len(valarr)):
+            temp2.data = valarr[i]
+            temp2 = temp2.next
+        return head
+
+
+#{ 
+ # Driver Code Starts
+# Node Class    
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+
+def printList(head):
+    tmp = head
+    while tmp:
+        print(tmp.data, end=' ')
+        tmp=tmp.next
+    print()
+
+if __name__=='__main__':
+    for i in range(int(input())):
+        n = int(input())
+        arr = [int(x) for x in input().split()]
+        
+        lis = Linked_List()
+        for i in arr:
+            lis.insert(i)
+        
+        newHead = Solution().reverseList(lis.head)
+        printList(newHead)
+
+# } Driver Code Ends
