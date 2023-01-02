58d00ed63dfbb4a6bfd80f5a1345690b12c3b113
Jayanth Menon S <83857943+mava123-0@users.noreply.github.com>
2023-01-02 18:34:53 +0530

    0-1 Knap Sack

---
+++ Back Tracking/BT_KnapSack.py
@@ -0,0 +1,32 @@
+res = []
+
+def solution(weights,benefits,index,n,tabs,mem):
+
+    if(index<0 or n==0):
+        return 0
+    if(weights[index]>n):
+        return 0
+
+    k_included = benefits[index] + solution(weights,benefits,index-1,n-weights[index],tabs+'\t',mem)
+    print(f"{tabs}Index {index} included, total benefit is {k_included}")
+    k_excluded = solution(weights,benefits,index-1,n,tabs+'\t',mem)
+    print(f"{tabs}Index {index} not included, total benefit is {k_excluded}")
+
+    if(max(k_excluded,k_included) == k_included):
+        res.append(index)
+        print(f"{tabs}Index {index} is chosen")
+    if(max(k_excluded,k_included) == k_excluded):
+        print(f"{tabs}Index {index} is not chosen")
+
+    return max(k_included,k_excluded)
+
+def main():
+    benefits = [60, 100, 120]
+    weights = [10, 20, 30]
+    n = 50
+
+    mem = [-1 for i in range(len(weights))]
+    print("\nMaximum Benefit is:",solution(weights,benefits,len(weights)-1,n,"",mem))
+
+if __name__ == '__main__':
+    main()
